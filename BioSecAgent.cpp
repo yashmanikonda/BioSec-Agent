d21f73900e3f394af3b50c0b4cf87d499ae0cdf4
Yash Manikonda <90308705+yashmanikonda@users.noreply.github.com>
2024-04-30 06:04:00 +0530

    Update BioSecAgent.cpp
    Final Agent code with API calls

--- BioSecAgent.cpp
+++ BioSecAgent.cpp
@@ -3,6 +3,16 @@
 #include <sddl.h>
 #include <iostream>
 #include <string>
+#include <bcrypt.h>
+#include <sstream>
+#include <iomanip>
+#include <vector>
+#include <wininet.h>
+#include <thread> // Include the <thread> header for std::this_thread
+#include <chrono> // Include the <chrono> header for std::chrono
+
+
+#pragma comment(lib, "wininet.lib")
 
 // Global variable for directory path
 LPCWSTR lpDirectoryName = L"C:\\Users\\public\\SecureFolder-BioSec";
@@ -55,7 +65,187 @@ void LockFolder() {
     LocalFree(pSD);
 }
 
+std::string RequestAPICall() {
+    std::string response;
+
+    HINTERNET hInternet = InternetOpen(L"MyApp", INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);
+    if (hInternet != NULL) {
+        HINTERNET hConnect = InternetOpenUrl(hInternet, L"https://biosec-api-imtmoxa2zq-el.a.run.app/request", NULL, 0, INTERNET_FLAG_RELOAD, 0);
+        if (hConnect != NULL) {
+            // Read the response
+            char buffer[4096];
+            DWORD bytesRead;
+            while (InternetReadFile(hConnect, buffer, sizeof(buffer), &bytesRead) && bytesRead > 0) {
+                // Append the response to the string
+                response.append(buffer, bytesRead);
+            }
+            // Close the connection handle
+            InternetCloseHandle(hConnect);
+        }
+        else {
+            std::cerr << "Failed to open URL" << std::endl;
+        }
+        // Close the Internet handle
+        InternetCloseHandle(hInternet);
+    }
+    else {
+        std::cerr << "Failed to open Internet" << std::endl;
+    }
+
+    return response;
+}
+
+std::string BioResponseCall() {
+    std::string response;
+
+    HINTERNET hInternet = InternetOpen(L"MyApp", INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);
+    if (hInternet != NULL) {
+        HINTERNET hConnect = InternetOpenUrl(hInternet, L"https://biosec-api-imtmoxa2zq-el.a.run.app/check2", NULL, 0, INTERNET_FLAG_RELOAD, 0);
+        if (hConnect != NULL) {
+            // Read the response
+            char buffer[4096];
+            DWORD bytesRead;
+            while (InternetReadFile(hConnect, buffer, sizeof(buffer), &bytesRead) && bytesRead > 0) {
+                // Append the response to the string
+                response.append(buffer, bytesRead);
+            }
+            // Close the connection handle
+            InternetCloseHandle(hConnect);
+        }
+        else {
+            std::cerr << "Failed to open URL" << std::endl;
+        }
+        // Close the Internet handle
+        InternetCloseHandle(hInternet);
+    }
+    else {
+        std::cerr << "Failed to open Internet" << std::endl;
+    }
+
+    return response;
+}
+
+void BioResponseCallCon() {
+    while (true) {
+        // Call the biometric API
+        std::string apiResponse = BioResponseCall();
+
+        // Check the response and execute the appropriate function
+        if (!apiResponse.empty() && apiResponse == "correct") {
+            UnLockFolder();
+            //SendStringOverPipe(hPipe, "BioMetric is correct and Folder is Unlocked");
+        }
+        else if (!apiResponse.empty() && apiResponse == "wrong") {
+            LockFolder();
+        }
+        else {
+            // Wait for 1 second before the next API call
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+    }
+}
+
+void SendStringOverPipe(HANDLE hPipe, const std::string& message) {
+    DWORD bytesWritten;
+    if (!WriteFile(hPipe, message.c_str(), message.size(), &bytesWritten, NULL)) {
+        std::cerr << "Error writing to pipe" << std::endl;
+    }
+    else {
+        std::cout << "Message sent over the pipe: " << message << std::endl;
+    }
+}
+
+std::string generateSHA256HashS(const std::string& input) {
+    HCRYPTPROV hProv;
+    HCRYPTHASH hHash;
+
+    if (!CryptAcquireContext(&hProv, nullptr, nullptr, PROV_RSA_AES, CRYPT_VERIFYCONTEXT)) {
+        std::cerr << "Error acquiring cryptographic context" << std::endl;
+        return "";
+    }
+
+    if (!CryptCreateHash(hProv, CALG_SHA_256, 0, 0, &hHash)) {
+        std::cerr << "Error creating hash object" << std::endl;
+        CryptReleaseContext(hProv, 0);
+        return "";
+    }
+
+    if (!CryptHashData(hHash, reinterpret_cast<const BYTE*>(input.c_str()), input.length(), 0)) {
+        std::cerr << "Error hashing data" << std::endl;
+        CryptDestroyHash(hHash);
+        CryptReleaseContext(hProv, 0);
+        return "";
+    }
+
+    DWORD hashSize = 0;
+    DWORD dataSize = sizeof(DWORD);
+    if (!CryptGetHashParam(hHash, HP_HASHSIZE, reinterpret_cast<BYTE*>(&hashSize), &dataSize, 0)) {
+        std::cerr << "Error getting hash size" << std::endl;
+        CryptDestroyHash(hHash);
+        CryptReleaseContext(hProv, 0);
+        return "";
+    }
+
+    std::vector<BYTE> hashData(hashSize, 0);
+    if (!CryptGetHashParam(hHash, HP_HASHVAL, hashData.data(), &hashSize, 0)) {
+        std::cerr << "Error getting hash value" << std::endl;
+        CryptDestroyHash(hHash);
+        CryptReleaseContext(hProv, 0);
+        return "";
+    }
+
+    CryptDestroyHash(hHash);
+    CryptReleaseContext(hProv, 0);
+
+    std::stringstream ss;
+    for (BYTE byte : hashData) {
+        ss << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(byte);
+    }
+
+    return ss.str();
+}
+
+
+bool verifyPassword(const std::string& inputPassword) {
+    // Read stored hash from registry
+    HKEY hKey;
+    LPCWSTR subKey = L"Software\\PBioword";
+    if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, subKey, 0, KEY_READ, &hKey) != ERROR_SUCCESS) {
+        std::cerr << "Error opening registry key" << std::endl;
+        return false;
+    }
+
+    DWORD dataSize = 0;
+    if (RegQueryValueEx(hKey, L"PasswordHash", 0, nullptr, nullptr, &dataSize) != ERROR_SUCCESS) {
+        RegCloseKey(hKey);
+        std::cerr << "Error querying value size or value not found" << std::endl;
+        return false;
+    }
+
+    std::vector<BYTE> hashData(dataSize, 0);
+    if (RegQueryValueEx(hKey, L"PasswordHash", 0, nullptr, hashData.data(), &dataSize) != ERROR_SUCCESS) {
+        RegCloseKey(hKey);
+        std::cerr << "Error reading hash from registry" << std::endl;
+        return false;
+    }
+
+    RegCloseKey(hKey);
+
+    std::string storedHash(reinterpret_cast<char*>(hashData.data()), dataSize);
+
+    // Hash the input password
+    std::string inputHash = generateSHA256HashS(inputPassword);
+
+    std::cout << "Input Hash: " << inputHash << std::endl;
+    std::cout << "Stored Hash: " << storedHash << std::endl;
+
+    // Compare the hashes
+    return (inputHash == storedHash);
+}
+
+
 int main() {
+    std::thread bioThread(BioResponseCallCon);
     // Define security descriptor
     SECURITY_DESCRIPTOR sd;
     InitializeSecurityDescriptor(&sd, SECURITY_DESCRIPTOR_REVISION);
@@ -83,50 +273,58 @@ int main() {
 
         if (hPipe == INVALID_HANDLE_VALUE) {
             std::cerr << "Failed to create named pipe" << std::endl;
-            continue; // Try again in the next iteration
+            return 1;
         }
 
         std::cout << "Named pipe server created and waiting for connections..." << std::endl;
 
         // Wait for client connection
-        if (ConnectNamedPipe(hPipe, NULL) != FALSE) {
+        if (ConnectNamedPipe(hPipe, NULL) != FALSE || GetLastError() == ERROR_PIPE_CONNECTED) {
             std::cout << "Client connected" << std::endl;
 
+            // Handle communication with client
             while (true) {
                 // Receive command from client
                 char buffer[1024];
                 DWORD bytesRead;
                 if (ReadFile(hPipe, buffer, sizeof(buffer), &bytesRead, NULL) != FALSE) {
-                    // Check if command is "lock" or "unlock"
-                    std::string command(buffer, bytesRead);
-                    if (command == "p") {
-                        // Execute lock method logic
-                        std::cout << "Lock command received" << std::endl;
-                        LockFolder();
-                    }
-                    else if (command == "b") {
-                        // Execute unlock method logic
-                        std::cout << "BioMetric Functionality not implemented yet" << std::endl;
-                        //UnLockFolder();
+                    // Check if command is "b" for biometric
+                    if (bytesRead > 0 && buffer[0] == 'b') {
+                        std::cout << "Biometric authentication received. Request is sent" << std::endl;
+                        std::string apiResponse = RequestAPICall();
+                        // Call LockFolder() here for biometric authentication
                     }
                     else {
-                        std::cerr << "Unknown command received: " << command << std::endl;
+                        // Extract the received password (excluding the command character)
+                        std::string password(buffer, buffer + bytesRead);
+                        // Verify the received password
+                        if (verifyPassword(password)) {
+                            std::cout << "Password verified. Lock method logic executed." << std::endl;
+                            UnLockFolder();
+                        }
+                        else {
+                            std::cerr << "Incorrect password received. Lock method logic not executed." << std::endl;
+                            LockFolder();
+                        }
                     }
                 }
                 else {
-                    // Log error and attempt to reconnect
-                    std::cerr << "Error reading from pipe" << std::endl;
-                    break;
+                    DWORD dwError = GetLastError();
+                    if (dwError != ERROR_PIPE_CONNECTED && dwError != ERROR_NO_DATA) {
+                        std::cerr << "Error reading from pipe: " << dwError << std::endl;
+                    }
+                    break; // Break out of the loop and wait for the next client connection
                 }
             }
+
+            // Close the named pipe handle after finishing communication with the client
+            CloseHandle(hPipe);
         }
         else {
             std::cerr << "Error connecting to client" << std::endl;
         }
-
-        // Close the named pipe handle before trying again
-        CloseHandle(hPipe);
     }
 
+    bioThread.join();
     return 0;
 }
